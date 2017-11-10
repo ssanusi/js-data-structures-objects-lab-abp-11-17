10406130912f372872ac1577da9a53ae2be28037
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-10 20:01:33 +0000

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromDriverByKey(driver, key){
   return anObj;
 }
 
-function destructivelyDeleteFromdriverByKey(driver, key){
+function destructivelyDeleteFromDriverByKey(driver, key){
   delete driver[key];
   return driver;
 }
